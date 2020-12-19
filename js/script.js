34ea3098f27882fd8b253e6edb9eef0823c5ed36
lindng <lindang1504@hotmail.com>
2020-12-19 19:57:19 +0100

    Done

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Question 1
+
+var number = 8;
+var boolean = true;
+var string = "Hello again";
+
+
+// Question 2
+
+var firstName = "Linda";
+var lastName = "Nguyen";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+
+// Question 3
+
+var pet = "frog";
+var typeOfPet = typeof pet;
+
+var type = typeof("frog");
+console.log ("The type of frog is" + " " + typeOfPet)
+
+
+// Question 4 
+var orderHasShipped = true;
+
+if (orderHasShipped === true) {
+    console.log("The order shipped");
+} else {
+    console.log("The order did not ship")
+}
+
+
+// Question 5
+
+for(var i = 7; i <=13; i++) {
+    console.log(i);
+}
